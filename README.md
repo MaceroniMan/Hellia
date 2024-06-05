61e22782c77567062fc908fae2dd5e30b7dbd82f
Maceroni Man <jackneils2024@gmail.com>
2024-06-05 16:16:02 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hellia
+A text based story game.
--- src/scripts/dialouge.ts
+++ src/scripts/dialouge.ts
@@ -8,7 +8,7 @@ export function dialogue(npc: any, game: any): void {
     let prev: string[] | null = null;
 
     while (true) {
-        console.log(game.scr.clear);
+        console.log(game.tm.clear);
 
         if (!(talkLocation in dialogueDict)) {
             throw new Error(`${talkLocation} does not exist`);
@@ -31,18 +31,18 @@ export function dialogue(npc: any, game: any): void {
         prev = null;
 
         for (let personIndex = 0; personIndex < dialogues.length; personIndex++) {
-            console.log(game.scr.clear);
-            console.log(Utils.replaceInStrings(beforeText, game.player, game.scr), "", true);
+            console.log(game.tm.clear);
+            console.log(Utils.replaceInStrings(beforeText, game.player, game.tm), "", true);
 
             const person = dialogues[personIndex];
             const text = Utils.wordWrap(`  "${person[1].replace("`", '"`  "')}"`, "`", "\n   ");
             console.log(`${person[0]}: `, "\n", true);
-            const doneSkip = game.scr.typing(text, game.player);
+            const doneSkip = game.tm.typing(text, game.player);
 
             if (doneSkip) {
-                console.log(game.scr.clear);
-                console.log(Utils.replaceInStrings(beforeText, game.player, game.scr), "", true);
-                console.log(`${person[0]}: \n${Utils.replaceInStrings(text, game.player, game.scr).replace("`", "\n")}`, "", true);
+                console.log(game.tm.clear);
+                console.log(Utils.replaceInStrings(beforeText, game.player, game.tm), "", true);
+                console.log(`${person[0]}: \n${Utils.replaceInStrings(text, game.player, game.tm).replace("`", "\n")}`, "", true);
             }
 
             beforeText += `${person[0]}: \n${text.replace("`", "\n")}\n\n`;
@@ -59,15 +59,15 @@ export function dialogue(npc: any, game: any): void {
             break;
         } else if (doExt !== "NA") {
             prompt();
-            console.log(game.scr.clear);
+            console.log(game.tm.clear);
             if (game.player.quests[doExt] === 0) {
-                game.scr.typing(`Quest '${game.quests[doExt].name}' received`, game.player, 0.1);
+                game.tm.typing(`Quest '${game.quests[doExt].name}' received`, game.player, 0.1);
             } else if (game.player.quests[doExt] === game.quests[doExt].points.length) {
-                game.scr.typing(`Quest '${game.quests[doExt].name}' completed`, game.player, 0.1);
+                game.tm.typing(`Quest '${game.quests[doExt].name}' completed`, game.player, 0.1);
             } else {
-                game.scr.typing(`Quest '${game.quests[doExt].name}' advanced`, game.player, 0.1);
+                game.tm.typing(`Quest '${game.quests[doExt].name}' advanced`, game.player, 0.1);
             }
-            game.scr.wait();
+            game.tm.wait();
         }
 
         if (dialogueCurrent.options.length === 0) {
@@ -86,14 +86,14 @@ export function dialogue(npc: any, game: any): void {
                 }
             }
 
-            beforeText = Utils.replaceInStrings(beforeText, game.player, game.scr).replace("`", "\n");
-            const dialogueMenu = new Menu.Menu(beforeText + optionString, game.scr, optionList, niceList);
+            beforeText = Utils.replaceInStrings(beforeText, game.player, game.tm).replace("`", "\n");
+            const dialogueMenu = new Menu.Menu(beforeText + optionString, game.tm, optionList, niceList);
 
-            game.scr.hideCursor()
+            game.tm.hideCursor()
             dialogueMenu.run({
-                keyInput: game.scr.getChar,
+                keyInput: game.tm.getChar,
                 writeFunc: (content) => {
-                    console.log(game.scr.clear + content);
+                    console.log(game.tm.clear + content);
                 },
                 onEnter: (value, niceValue) => {
                     talkLocation = value.split("$")[0];
